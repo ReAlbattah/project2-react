f6e68514f43d4540d02bbd5631957dc8eddef886
ReAlbattah <47796658+ReAlbattah@users.noreply.github.com>
2022-06-27 16:28:10 +0300

    Add files via upload

---
+++ src/Pages/Quiz/Quiz.js
@@ -0,0 +1,56 @@
+import { CircularProgress } from "@material-ui/core";
+import { useEffect, useState } from "react";
+import Question from "../../components/Question/Question";
+
+const Quiz = ({ name, questions, score, setScore, setQuestions }) => {
+  const [options, setOptions] = useState();
+  const [currQues, setCurrQues] = useState(0);
+
+  useEffect(() => {
+    setOptions(
+      questions &&
+        handleShuffle([
+          questions[currQues]?.correct_answer,
+          ...questions[currQues]?.incorrect_answers,
+        ])
+    );
+  }, [currQues, questions]);
+
+  console.log(questions);
+
+  const handleShuffle = (options) => {
+    return options.sort(() => Math.random() - 0.5);
+  };
+
+  return (
+    <div >
+      <span >Hello {name}</span>
+
+      {questions ? (
+        <>
+          <div >
+            <span>{questions[currQues].category}</span>
+            <span>
+              Score : {score}
+            </span>
+          </div>
+          <Question
+            currQues={currQues}
+            setCurrQues={setCurrQues}
+            questions={questions}
+            options={options}
+            correct={questions[currQues]?.correct_answer}
+            score={score}
+            setScore={setScore}
+            setQuestions={setQuestions}
+          />
+        </>
+      ) : (
+        <CircularProgress
+        />
+      )}
+    </div>
+  );
+};
+
+export default Quiz;
